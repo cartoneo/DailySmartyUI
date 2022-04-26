ed4f5f9739a13064ac37183cbb06140636adb92a
Eddy Lozano <eddylozano25@yahoo.com>
2022-04-26 16:58:46 -0600

    DailySmartyImgLogo

--- src/components/app.js
+++ src/components/app.js
@@ -1,10 +1,13 @@
 import React, { Component } from 'react';
+import Logo from "./logo";
 
 export default class App extends Component {
   render() {
     return (
       <div className='app'>
-        <h1>Logo</h1>
+        <h1>
+          <Logo/>
+        </h1>
       </div>
     );
   }
